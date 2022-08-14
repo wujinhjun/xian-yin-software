aed3714f37c22099c48715a47fb1720e24511dd7
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-08-14 23:11:50 +0800

    feat: environment config

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
+const isDev = require('electron-is-dev');
+
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    // 加载 index.html
+    const urlLocation = isDev ? "http://localhost:3000" : `url`;
+    mainWindow.loadURL(urlLocation)
+
+    // 打开开发工具
+    mainWindow.webContents.openDevTools()
+    installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], options)
+        .then((name) => console.log(`Added Extension: ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+}
+
+const options = {
+    loadExtensionOptions: { allowFileAccess: true },
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
