126ce27d1c5f59b5757d47fb5aaf683da5f22b1e
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-08-16 01:04:30 +0800

    feat: sidebar perfect layout

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,9 +1,11 @@
 import {
+  SideContainer,
   SideWrapper,
   SideButtonList,
   SideButtonWrapper,
   ButtonIcon,
   ButtonTitle,
+  SideBottom,
 } from "./style";
 
 import dailyIcon from "../../static/svg/clipboard.svg";
@@ -12,22 +14,26 @@ import settingIcon from "../../static/svg/settings.svg";
 
 const SideBar = () => {
   return (
-    <SideWrapper>
-      <SideButtonList>
-        <SideButtonWrapper className="active">
-          <ButtonIcon icon={dailyIcon}></ButtonIcon>
-          <ButtonTitle>日程</ButtonTitle>
-        </SideButtonWrapper>
-        <SideButtonWrapper>
-          <ButtonIcon icon={checkIcon}></ButtonIcon>
-          <ButtonTitle>ToDo</ButtonTitle>
-        </SideButtonWrapper>
-        <SideButtonWrapper>
-          <ButtonIcon icon={settingIcon}></ButtonIcon>
-          <ButtonTitle>设置</ButtonTitle>
-        </SideButtonWrapper>
-      </SideButtonList>
-    </SideWrapper>
+    <SideContainer>
+      <SideWrapper>
+        <SideButtonList>
+          <SideButtonWrapper className="active">
+            <ButtonIcon icon={checkIcon}></ButtonIcon>
+            <ButtonTitle>ToDo</ButtonTitle>
+          </SideButtonWrapper>
+          <SideButtonWrapper>
+            <ButtonIcon icon={dailyIcon}></ButtonIcon>
+            <ButtonTitle>日程</ButtonTitle>
+          </SideButtonWrapper>
+        </SideButtonList>
+        <SideBottom>
+          <SideButtonWrapper id="settings">
+            <ButtonIcon icon={settingIcon}></ButtonIcon>
+            <ButtonTitle>设置</ButtonTitle>
+          </SideButtonWrapper>
+        </SideBottom>
+      </SideWrapper>
+    </SideContainer>
   );
 };
 
--- src/components/SideBar/style.jsx
+++ src/components/SideBar/style.jsx
@@ -7,25 +7,38 @@ import {
   whiteColor,
 } from "../../styleVariable";
 
-export const SideWrapper = styled.div`
-  box-sizing: border-box;
+export const SideContainer = styled.div`
   width: 128px;
-  height: 100%;
-  position: fixed;
+  flex-grow: 0;
+`;
+
+export const SideWrapper = styled.div`
+  width: 128px;
+  box-sizing: border-box;
+  position: absolute;
   top: 82px;
   bottom: 0;
   background: ${backColor};
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  /* flex: 1; */
+  flex-grow: 0;
 `;
 
 export const SideButtonList = styled.div`
-  padding-top: 12px;
+  padding-top: 36px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-items: center;
-  flex: 1;
+  gap: 24px;
+`;
+
+export const SideBottom = styled.div`
+  position: relative;
+  bottom: 24px;
 `;
 
 export const SideButtonWrapper = styled.div`
@@ -35,18 +48,17 @@ export const SideButtonWrapper = styled.div`
   box-sizing: border-box;
   padding: 8px 16px;
   margin: 0 auto;
-  margin-top: 24px;
+  /* margin-top: 24px; */
   height: 44px;
   width: 114px;
   background: ${backColor};
+  cursor: pointer;
   &:hover {
     background: ${mainColor};
   }
   &.active {
     background: ${mainColor};
   }
-
-  cursor: pointer;
 `;
 
 export const ButtonIcon = styled.div`
