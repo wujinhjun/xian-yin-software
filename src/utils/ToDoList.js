cf7ff58b7a80f5ce96dc9c79d78fb8bcefa465fb
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-08-16 23:55:20 +0800

    feat: mock data

---
+++ src/utils/ToDoList.js
@@ -0,0 +1,95 @@
+export const ToDoData = [
+    {
+        "id": 0,
+        "title": "To Do",
+        "count": 3,
+        "tasks": [
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            },
+            {
+                "id": 0,
+                "name": "hello to do",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.23"
+            },
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            }
+        ]
+    },
+    {
+        "id": 0,
+        "title": "Doing",
+        "count": 3,
+        "tasks": [
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            },
+            {
+                "id": 0,
+                "name": "hello to do",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.23"
+            },
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            }
+        ]
+    },
+    {
+        "id": 0,
+        "title": "Done",
+        "count": 3,
+        "tasks": [
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            },
+            {
+                "id": 0,
+                "name": "hello to do",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.23"
+            },
+            {
+                "id": 0,
+                "name": "hello",
+                "isActive": false,
+                "contents": [{ "id": 0, "content": "hello", "isCompleted": false }],
+                "startDate": "2022.8.16",
+                "endDate": "2022.8.22"
+            }
+        ]
+    }
+]
