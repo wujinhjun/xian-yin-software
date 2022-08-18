aa441c0ea21769fa956ac6f614a7ec14c3e644ae
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-08-18 15:45:50 +0800

    feat: change some icon and color

--- src/styleVariable.js
+++ src/styleVariable.js
@@ -3,7 +3,7 @@ export const backColor = "#4E7BB8";
 export const lightColor = "#7CABE7";
 export const whiteColor = "#ffffff";
 export const grayColor = "#ECECEC";
-export const grayMoreColor = "#5A5A5A"
+export const grayMoreColor = "#999999";
 export const blackColor = "#000000";
 export const assistColor = "#4cc0e4";
 
